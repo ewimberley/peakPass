f61348be99c8713fd637b601e17ff84992add626
Eric Wimberley <blank89@gmail.com>
2018-05-28 16:44:08 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #prerequisites:
-sudo apt-get install r-base bedtools python bowtie texlive texlive-full build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev python-pip
-#sudo Rscript install.R
+sudo apt-get -y install r-base bedtools python bowtie texlive texlive-full build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev python-pip samtools
+sudo Rscript install.R
--- snrTreatmentTester/compareTreatments.sh
+++ snrTreatmentTester/compareTreatments.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#Usage: compareTreatments.sh [data directory] [reads file]
 
 #Run this command first:
-#export R_LIBS="/thesis/R_libs"
+export R_LIBS="~/R_libs"
 
-Rscript run_spp.R -c=chipSampleRep1.tagAlign.gz -i=controlSampleRep0.tagAlign.gz -npeak=300000 -odir=./peaks/reps -savr -savp -rf -out=./stats/phantomPeakStatsReps.tab
+#Rscript run_spp.R -c=chipSampleRep1.tagAlign.gz -i=controlSampleRep0.tagAlign.gz -npeak=300000 -odir=./peaks/reps -savr -savp -rf -out=./stats/phantomPeakStatsReps.tab
+Rscript run_spp.R -c=$1/$2.bam -odir=./peaks/reps -savp -rf -out=./$2.tab
