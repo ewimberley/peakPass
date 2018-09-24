40da9f01d118e6d134d0587cec8f64ef665086c8
Eric Wimberley <blank89@gmail.com>
2018-09-23 20:23:10 -0400

    Hypothesis tesing for RSC gain

--- snrTreatmentTester/qualityGainByTreatmentGraph.R
+++ snrTreatmentTester/qualityGainByTreatmentGraph.R
@@ -7,9 +7,13 @@ qualityDataFile <- args[1]
 qualityData <- read.csv(qualityDataFile, header = TRUE)
 qualityDataShaped <- melt(qualityData,id.vars='treatmentName', measure.vars=c('rscGain'))
 
+hypTest <- t.test(value~treatmentName, qualityDataShaped)
+
+
 png(file = "RSCGainByTreatment.png")
 colnames(qualityDataShaped)[1] <- "Treatment"
 colnames(qualityDataShaped)[3] <- "GainRSC"
-ggplot(qualityDataShaped) + geom_boxplot(aes(x=Treatment, y=GainRSC)) + labs(title="Gain in RSC By Treatment") + theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5))
+p <- ggplot(qualityDataShaped) + geom_boxplot(aes(x=Treatment, y=GainRSC)) + labs(title="Gain in RSC By Treatment") + theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5))
+p + annotate("text", label=paste("p-value: ", hypTest$p.value), x=1, y=1)
 
 dev.off()
