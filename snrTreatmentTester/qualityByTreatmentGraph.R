674735767ab58cd149de7c0d9ec232f2d0d421be
Eric Wimberley <blank89@gmail.com>
2018-10-10 23:16:25 -0400

    40 percent blacklist, dataset includes phantom peak boolean column, violin plots

--- snrTreatmentTester/qualityByTreatmentGraph.R
+++ snrTreatmentTester/qualityByTreatmentGraph.R
@@ -3,8 +3,8 @@ library(reshape2)
 library(data.table)
 options(echo=TRUE)
 args <- commandArgs(trailingOnly = TRUE)
+qualityDataFile <- "/thesis/workspace/peakPass/snrTreatmentTester/completeData/all_data.csv"
 qualityDataFile <- args[1]
-#qualityDataFile <- "/thesis/workspace/peakPass/snrTreatmentTester/completeData/all_quality_data.csv"
 qualityData <- read.csv(qualityDataFile, header = TRUE)
 melted <- melt(qualityData)
 nscData <- melted[melted$variable %like% "NSC", ]
@@ -18,12 +18,11 @@ detach(nscData)
 png(file = "NSCByTreatment.png")
 colnames(nscData)[2] <- "Treatment"
 colnames(nscData)[3] <- "NSC"
-p <- ggplot(nscData) + geom_violin(aes(x=Treatment, y=NSC))
-p <- p + geom_boxplot(aes(x=Treatment, y=NSC), width=0.1)
-p <- p + labs(title="NSC By Treatment") + theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5))
-#p <- p + scale_y_continuous(limits = quantile(nscData$NSC, c(0.01, 0.9)))
+means <- aggregate(NSC ~  Treatment, nscData, mean)
+p <- ggplot(nscData) + geom_violin(aes(x=Treatment, y=NSC)) + geom_boxplot(aes(x=Treatment, y=NSC), width=0.1) + labs(title="NSC By Treatment") 
+p <- p + theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5)) + geom_text(data = means, aes(label = round(NSC, digits = 3), y = NSC, x = Treatment))
+p + scale_y_continuous(limits = quantile(nscData$NSC, c(0.01, 0.9)))
 #p <- p + annotate("text", label=paste("p-value: ", holmTest$p.value[1][1]), x=1, y=1)
-p
 dev.off()
 
 attach(rscData)
@@ -34,11 +33,10 @@ detach(rscData)
 png(file = "RSCByTreatment.png")
 colnames(rscData)[2] <- "Treatment"
 colnames(rscData)[3] <- "RSC"
+means <- aggregate(RSC ~  Treatment, rscData, mean)
 #p <- ggplot(rscData) + geom_boxplot(aes(x=Treatment, y=RSC)) + labs(title="RSC By Treatment") + theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5))
-p <- ggplot(rscData) + geom_violin(aes(x=Treatment, y=RSC))
-p <- p + geom_boxplot(aes(x=Treatment, y=RSC), width=0.1)
-p <- p + labs(title="RSC By Treatment") + theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5))
-#p <- p + scale_y_continuous(limits = quantile(rscData$RSC, c(0.1, 0.9)))
-#p + annotate("text", label=paste("p-value: ", holmTest$p.value), x=1, y=1)
-p
+p <- ggplot(rscData) + geom_violin(aes(x=Treatment, y=RSC)) + geom_boxplot(aes(x=Treatment, y=RSC), width=0.1) + labs(title="RSC By Treatment") 
+p + theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5)) + geom_text(data = means, aes(label = round(RSC, digits = 3), y = RSC, x = Treatment))
+# + scale_y_continuous(limits = quantile(rscData$RSC, c(0.1, 0.9)))
+# + annotate("text", label=paste("p-value: ", holmTest$p.value[1][1]), x=2, y=1)
 dev.off()
