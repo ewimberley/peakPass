aa470b0ecfe014291507a90adec306724ba13f65
Eric Wimberley <wimberleyeric@gmail.com>
2021-02-11 19:42:12 -0500

    Missed one.

--- featurePipeline/configurations/tair10_ewimberley_500bp.sh
+++ featurePipeline/configurations/tair10_ewimberley_500bp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export GENOME_VERSION="tair10"
-export BLACKLIST_BED="$BLACKLISTS/tair10-ewimberley-excludedlist.bed"
+export BLACKLIST_BED="$EXCLUDED/tair10-ewimberley-excludedlist.bed"
 export WINDOW_SIZE=500
 export OVERLAP_WINDOW=0.7
 export NUM_SAMPLES=4000000
